e33bf34e8658c0b0320ccdba592dffbd882251c7
Tilak Singh <tsingh99991@gmail.com>
2024-07-16 20:44:34 +0530

    just msg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="">
-      hey
+    <main className=" flex flex-row justify-center items-center font-mono text-lg">
+     fuck u bitch
     </main>
   );
 }
